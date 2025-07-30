88b1c445afdd52a673ebc651dc1017e67357d58c
Rohit Patnaik <quanticle@gmail.com>
2025-07-30 03:47:10 -0500

    Added option to open the archive search page
    This adds an option to open the archive search page rather than going directly
    to the latest archived version. This enables the user to select a previous
    version, in case the latest version has captured a block page or paywall.

--- js/background.js
+++ js/background.js
@@ -13,11 +13,26 @@ async function getArchiveHostname() {
   return archiveHostname;
 }
 
+async function getOpenSearchPageOption() {
+  let settings = (await browser.storage.local.get("settings")).settings;
+  if (settings && settings["open_search_page"]) {
+    return true;
+  }
+  return false;
+}
+
 browser.contextMenus.onClicked.addListener(async (info, tab) => {
   let linkUrl = new URL(info.linkUrl);
   let strippedUrl = new URL(`${linkUrl.protocol}//${linkUrl.hostname}${linkUrl.pathname}`);
   let archiveHostname = await getArchiveHostname();
-  let archiveUrl = `https://${archiveHostname}/timegate/${strippedUrl}`;
+  let openSearchPage = await getOpenSearchPageOption();
+  let archiveUrl = "";
+  if (openSearchPage){
+    archiveUrl = `https://${archiveHostname}/${strippedUrl}`;
+  }
+  else {
+    archiveUrl = `https://${archiveHostname}/timegate/${strippedUrl}`;
+  }
   if(info.menuItemId === "archive_context_menu") {
     browser.tabs.create({url: archiveUrl,
                          openerTabId: tab.id});
--- options/options.js
+++ options/options.js
@@ -6,10 +6,18 @@ document.addEventListener('DOMContentLoaded', async (_) => {
   else {
     document.getElementById("archive_hostname").value = "archive.is";
   }
+  if (settings && settings["open_search_page"]) {
+    document.getElementById("open_search_page_true").checked = true;
+  }
+  else {
+    document.getElementById("open_search_page_false").checked = true;
+  }
   document.getElementById("archive_hostname_form").addEventListener('submit', (ev) => {
     ev.preventDefault();
-    let new_archive_hostname = document.getElementById("archive_hostname").value;
+    let newArchiveHostname = document.getElementById("archive_hostname").value;
+    let openSearchPage = document.querySelector('input[name="open_search_page"]:checked').value
     browser.storage.local.set({"settings": 
-                               {"archive_host_name": new_archive_hostname}});
+                               {"archive_host_name": newArchiveHostname,
+                                "open_search_page": openSearchPage}});
   });
 });
